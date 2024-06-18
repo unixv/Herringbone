16481376dce8b3c335952523da03644e8e6426d9
unixv <58447354+unixv@users.noreply.github.com>
2024-06-17 22:49:50 -0500

    Add files via upload

--- herringbone/herringbone.py
+++ herringbone/herringbone.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from bson import ObjectId
 
@@ -18,18 +18,50 @@ def home():
 
 @app.route('/events')
 def events():
-    events = events_collection.find().sort("logid", -1)
-    events_list = list(events)
-    return render_template('events.html', events=events_list)
+    return render_template('events.html')
 
 @app.route('/api/events')
 def api_events():
-    events = events_collection.find().sort("logid", -1)
+    draw = request.args.get('draw', type=int)
+    start = request.args.get('start', type=int)
+    length = request.args.get('length', type=int)
+    search_value = request.args.get('search[value]', type=str)
+    order_column_index = request.args.get('order[0][column]', type=int)
+    order_column_name = request.args.get(f'columns[{order_column_index}][data]')
+    order_direction = request.args.get('order[0][dir]')
+    order = 1 if order_direction == 'asc' else -1
+
+    # Total records count
+    total_records = events_collection.count_documents({})
+
+    # Filtered records
+    if search_value:
+        query = {"$or": [
+            {"source_address": {"$regex": search_value, "$options": "i"}},
+            {"source_port": {"$regex": search_value, "$options": "i"}},
+            {"message": {"$regex": search_value, "$options": "i"}},
+            {"type": {"$regex": search_value, "$options": "i"}},
+            {"indicators": {"$regex": search_value, "$options": "i"}},
+            {"logid": {"$regex": search_value, "$options": "i"}}
+        ]}
+    else:
+        query = {}
+
+    filtered_records = events_collection.count_documents(query)
+    events = events_collection.find(query).sort(order_column_name, order).skip(start).limit(length)
+
     events_list = []
     for event in events:
         event['_id'] = str(event['_id'])
         events_list.append(event)
-    return jsonify(events_list)
+
+    response = {
+        'draw': draw,
+        'recordsTotal': total_records,
+        'recordsFiltered': filtered_records,
+        'data': events_list
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
