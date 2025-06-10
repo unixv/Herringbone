8062bef0662e70c9f1004fb7d1e5d71458610373
unixv <58447354+unixv@users.noreply.github.com>
2025-06-10 13:43:25 -0500

    Add files via upload

---
+++ logingestion/receiver/database.py
@@ -0,0 +1,49 @@
+from pymongo import MongoClient
+from datetime import datetime
+import os
+import json
+
+class MongoNotSet(Exception):
+    """If the MONGO_HOST is not set in the container environment variables"""
+    pass
+
+class mongo_database():
+
+    def __init__(self, collection_name):
+        self.MONGO_HOST = os.environ.get('MONGO_HOST', None)
+        self.DB_NAME = 'herringbone_db'
+        self.COLLECTION_NAME = collection_name
+        self.MONGO_USER = os.environ.get('MONGO_USER')
+        self.MONGO_PASS = os.environ.get('MONGO_PASS')
+
+        uri_parts = self.MONGO_HOST.split("://")
+        if len(uri_parts) != 2:
+            raise Exception("Invalid MONGO_HOST format. Expecting mongodb://hostname:port")
+        
+        self.AUTH_URI = f"{uri_parts[0]}://{self.MONGO_USER}:{self.MONGO_PASS}@{uri_parts[1]}"
+
+        if self.MONGO_HOST is not None:
+            self.client = MongoClient(self.MONGO_HOST)
+            self.db = self.client[self.DB_NAME]
+            self.collection = self.db[self.COLLECTION_NAME]
+
+        else:
+            raise MongoNotSet("MONGO_HOST is not set in the container environment variables.")
+        
+        try:
+            self.client = MongoClient(self.AUTH_URI)
+            self.db = self.client[self.DB_NAME]
+            self.collection = self.db[self.COLLECTION_NAME]
+
+        except Exception as e:
+            raise Exception(f"Failed to connect to MongoDB: {e}")
+        
+    def insert_log(self, log_object):
+        try:
+            result = self.collection.insert_one(log_object)
+            print(f"[✓] Inserted log with _id: {result.inserted_id}")
+        except Exception as e:
+            print(f"[✗] Error inserting log: {e}")
+
+    def close(self):
+        self.client.close()
--- logingestion/receiver/inet.py
+++ logingestion/receiver/inet.py
@@ -1,7 +1,11 @@
 import socket
 import os
+from database import MongoDatabaseHandler
 
 def start_udp_receiver():
+    print("Connecting to database...")
+    mongo = MongoDatabaseHandler()
+
     print("Receiver type set to UDP...")
     udp_receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_receiver.bind(('0.0.0.0', 7002))
@@ -11,10 +15,11 @@ def start_udp_receiver():
         data, addr = udp_receiver.recvfrom(1024)
         data = data.decode('utf-8')
         print(f"[Source Address: {addr}] {data}")
+        mongo.insert_log({"source_address": addr, "log": data})
 
 def start_tcp_receiver():
     print("Receiver type set to TCP...")
-    tcp_receiver = socker.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_receiver.bind(('0.0.0.0', 7002))
     print("Started on container port 7002")
 
